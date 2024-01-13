a6377da6be011b77a2f3382116ddfd88394510e6
Buz Carter <buzcarter@gmail.com>
2024-01-13 09:10:49 -0800

    prop up the demo

---
+++ include/Critters.h
@@ -0,0 +1,55 @@
+#ifndef CRITTERS_H
+#define CRITTERS_H
+
+#include <iostream>
+#include <string>
+#include <cstdint>
+
+using std::cout;
+using std::endl;
+using std::to_string;
+
+class Critters
+{
+public:
+  static void blink()
+  {
+    static bool isOn = false;
+    cout << "blink: " << (isOn ? "ON" : "OFF") << endl;
+  }
+
+  static void fadeOut()
+  {
+    const uint8_t FADE_RATE = 5;
+    static uint8_t value = 100;
+
+    if (value == 0)
+    {
+      return;
+    }
+
+    value = (value < FADE_RATE) ? 0 : value - FADE_RATE;
+    cout << "fadeOut: " << to_string(value) << endl;
+  }
+
+  static void fade()
+  {
+    static uint8_t value = 0;
+    static uint8_t step = 5;
+
+    cout << "fade: " << to_string(value) << endl;
+
+    if (value == 0)
+    {
+      step = 5;
+    }
+    else if (value == 100)
+    {
+      step = -5;
+    }
+    value += step;
+  }
+};
+
+
+#endif
--- src/critter.cpp
+++ src/critter.cpp
@@ -1,13 +1,9 @@
 #include <chrono>
 #include <iostream>
-#include <string>
 #include <thread>
 
 #include "TaskTimer.h"
-
-using std::cout;
-using std::endl;
-using std::to_string;
+#include "Critters.h"
 
 #define CLOCK_INTERVAL 25
 #define PROGRAM_DURATION 2000
@@ -16,48 +12,6 @@ void delay(int ms)
 {
   std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
-
-class Critters
-{
-public:
-  static void blink()
-  {
-    static bool isOn = false;
-    cout << "blink: " << (isOn ? "ON" : "OFF") << endl;
-  }
-
-  static void fadeOut()
-  {
-    const uint8_t FADE_RATE = 5;
-    static uint8_t value = 100;
-
-    if (value == 0)
-    {
-      return;
-    }
-
-    value = (value < FADE_RATE) ? 0 : value - FADE_RATE;
-    cout << "fadeOut: " << to_string(value) << endl;
-  }
-
-  static void fade()
-  {
-    static uint8_t value = 0;
-    static uint8_t step = 5;
-
-    cout << "fade: " << to_string(value) << endl;
-
-    if (value == 0)
-    {
-      step = 5;
-    }
-    else if (value == 100)
-    {
-      step = -5;
-    }
-    value += step;
-  }
-};
 
 // defined here just for funsies
 TaskTimer *blinkTimer;
@@ -81,7 +35,7 @@ int main()
   int counter = 0;
   while (counter < PROGRAM_DURATION / CLOCK_INTERVAL)
   {
-    cout << "t" << counter << endl;
+    std::cout << "t" << counter << std::endl;
     counter++;
 
     blinkTimer->tick();
