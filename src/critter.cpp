eeb9e7cf1bed8dbbc7eef9f552654bd84283c7dd
Buz Carter <buzcarter@gmail.com>
2024-01-13 06:25:54 -0800

    let's keep this alive for a while;

--- src/critter.cpp
+++ src/critter.cpp
@@ -1,12 +1,26 @@
+#include <chrono>
 #include <iostream>
-#include <sandbox.h>
+#include <thread>
+#include "sandbox.h"
 
 using std::cout;
 using std::endl;
 
+#define CLOCK_INTERVAL 100
+#define PROGRAM_DURATION 2000
+
 int main()
 {
+  int counter = 0;
   cout << (isSwingSetAvailable() ? "Go to the park now" : "You'd better wait") << endl;
+
+  while (counter < PROGRAM_DURATION / CLOCK_INTERVAL)
+  {
+    cout << "Tick #" << counter << endl;
+    counter++;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
 
   return 0;
 }
